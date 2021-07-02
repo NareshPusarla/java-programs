42f472c4dc9b8a4d4fe356618c42fcf6e5b47e49
Naresh <pnareshsatya@gmail.com>
2021-07-02 07:56:45 +0530

    Leap year

--- src/com/bridgelabz/javabasics/LeapYear.java
+++ src/com/bridgelabz/javabasics/LeapYear.java
@@ -6,9 +6,17 @@ public static void main(String args[]){
 	System.out.println("Enter the year to check: ");
 	int year = scanner.nextInt();
 	if (year%4 == 0){
-		System.out.println("Leap Year: " + year);
+		if(year%100 == 0){
+			if(year%400 == 0){
+			System.out.println(year+" is a Leap year");
+			}else{
+				System.out.println(year+" is a not a leap year");
+			}
+		}else{
+		System.out.println(year+" is Leap Year");
+		}
 	}else{
-		System.out.println("Leap Year: " + year);
+		System.out.println(year+" is not a Leap Year");
 	}
 }
 }
